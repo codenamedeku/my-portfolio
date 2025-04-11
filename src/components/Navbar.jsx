921dcbabd11e0e26aa9b9c749871c5755f2dd163
codenamedeku <devaraja.work@gmail.com>
2025-04-11 15:33:12 +0700

    Update warna border pada Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
     }, []);
 
     return (
-        <header className="fixed top-0 w-full bg-white dark:bg-gray-950 text-gray-800 dark:text-white p-4 flex justify-between items-center z-50 border-b border-gray-200 dark:border-gray-700">
+        <header className="fixed top-0 w-full bg-white dark:bg-gray-950 text-gray-800 dark:text-white p-4 flex justify-between items-center z-50 border-b border-gray-200 dark:border-gray-800">
             {/* Left Side */}
             <div className="flex items-center gap-3">
                 <img src={avatar} alt="Avatar" className="w-8 h-8 rounded-full object-cover" />
@@ -65,7 +65,7 @@ const Navbar = () => {
             )}
 
             {/* Sidebar */}
-            <div className={`fixed top-0 right-0 h-full p-6 bg-white dark:bg-gray-950 text-gray-800 dark:text-white border-l border-gray-200 dark:border-gray-700 transform ${isSidebarOpen ? "translate-x-0" : "translate-x-full"} transition-transform duration-300 ease-in-out z-50`}>
+            <div className={`fixed top-0 right-0 h-full p-6 bg-white dark:bg-gray-950 text-gray-800 dark:text-white border-l border-gray-200 dark:border-gray-800 transform ${isSidebarOpen ? "translate-x-0" : "translate-x-full"} transition-transform duration-300 ease-in-out z-50`}>
                 <Sidebar closeSidebar={closeSidebar} disableAvatarScroll={true} />
             </div>
         </header>
